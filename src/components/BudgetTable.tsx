1b84843d6c0fc901d909b2e69577c9ed524d8b5d
romaincombret-creator <romain.combret@sbe-solution.com>
2025-08-15 12:02:42 -0400

    Fix BudgetTable component parsing error

--- src/components/BudgetTable.tsx
+++ src/components/BudgetTable.tsx
@@ -1,1 +1,16 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react';
+
+interface BudgetTableProps {
+  // Add props as needed
+}
+
+const BudgetTable: React.FC<BudgetTableProps> = () => {
+  return (
+    <div>
+      <h2>Budget Table</h2>
+      {/* Component content will be implemented based on requirements */}
+    </div>
+  );
+};
+
+export default BudgetTable;
